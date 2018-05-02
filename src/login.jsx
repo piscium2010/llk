b7634edb40a1473703abaaa12cfde656621e7bda
piscium2010 <32027562+piscium2010@users.noreply.github.com>
2018-05-02 14:48:27 +0800

    Update login.jsx

--- src/login.jsx
+++ src/login.jsx
@@ -2,38 +2,74 @@ import React from 'react'
 import classnames from 'classnames'
 import './login.less'
 
+class InputField extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            focus: false
+        }
+        this.onFocus = this.onFocus.bind(this)
+        this.onBlur = this.onBlur.bind(this)
+    }
+
+    render() {
+        const { label, type, className, errorMsg, ...restProps } = this.props
+        const _classname = classnames({
+            focus: this.state.focus
+        })
+
+        return(
+            <div className={`${className} ${_classname}`}>
+                <label>{label}</label>
+                <input type={type}
+                    className="input-field" 
+                    onFocus={this.onFocus} 
+                    onBlur={this.onBlur}
+                    {...restProps}
+                />
+                <u></u>
+                <div className='error'>{errorMsg}</div>
+            </div>
+        )
+    }
+
+    onFocus(evt) {
+        this.setState({focus:true})
+    }
+
+    onBlur(evt) {
+        if(!evt.target.value) {
+            this.setState({focus:false})
+        }
+    }
+}
+
 export default class Login extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            usernameFocus: false,
-            passwordFocus: false,
-            repeatPasswordFocus: false,
             level_login: true,
             level_reg: false,
+            level_forget: false,
+            shakeIfEmpty: false,
+            email: null,
+            password: null,
+            repeatPassword: null,
+            emailErrorMsg: 'email is already registered',
+            passwordErrorMsg: 'email is already registered',
+            repeatPasswordErrorMsg: 'email is already registered'
         }
-        this.onFocus = this.onFocus.bind(this)
-        this.onBlur = this.onBlur.bind(this)
+
+        this.formBoxRef = React.createRef()
+
         this.onClickRegister = this.onClickRegister.bind(this)
-    }
-
-    onFocus(name) {
-        return evt => {
-            this.setState({
-                [name]:true
-            })
-        }
-    }
-
-    onBlur(name) {
-        return evt => {
-            if(!evt.target.value){
-                this.setState({
-                    [name]: false
-                })
-            }
-        }
+        this.onClickForgot = this.onClickForgot.bind(this)
+        this.onClickBack = this.onClickBack.bind(this)
+        this.onChange = this.onChange.bind(this)
+        this.shakeIfEmpty = this.shakeIfEmpty.bind(this)
+        this.onGo = this.onGo.bind(this)
+        
     }
 
     onClickRegister() {
@@ -43,27 +79,54 @@ export default class Login extends React.Component {
         })
     }
 
+    onClickForgot(evt) {
+        evt.preventDefault()
+        this.setState({
+            level_forget: true,
+            level_reg: false
+        })
+    }
+
+    onClickBack(evt) {
+        evt.preventDefault()
+        this.setState({
+            level_forget: false,
+            level_login: true
+        })
+    }
+
+    onChange(name) {
+        return evt => {
+            this.setState({
+                [name]: evt.target.value
+            })
+        }
+    }
+
+    onGo() {
+        this.setState({
+            shakeIfEmpty: true
+        }, () => {
+            setTimeout(() => {
+                this.setState({
+                    shakeIfEmpty: false
+                })
+            }, 2000)
+        })
+    }
+
+    shakeIfEmpty(name) {
+        return !this.state.shakeIfEmpty || this.state[name] ? '' : 'shake'
+    }
+
+
     render() {
-        let userNameClassName = classnames({
-            f_row: true,
-            focus: this.state.usernameFocus
-        })
-
-        let passwordClassName = classnames({
-            f_row: true,
-            focus: this.state.passwordFocus
-        })
-        let repeatPasswordClassName = classnames({
-            f_row: true,
-            focus: this.state.repeatPasswordFocus
-        })
-
         let formBoxClassName = classnames({
             formBox: true,
             "level-login": this.state.level_login,
             "level-reg": this.state.level_reg,
-            "level-reg-reverse": this.state.level_reg
-
+            "level-reg-reverse": this.state.level_reg,
+            "level-forget": this.state.level_forget
         })
 
         return(
@@ -74,52 +137,28 @@ export default class Login extends React.Component {
                         <div className="box loginBox">
                         <h2>LOGIN</h2>
                         <form className="form">
-                            <div className={userNameClassName}>
-                            <label>Username</label>
-                            <input type="text" className="input-field" required onFocus={this.onFocus('usernameFocus')} onBlur={this.onBlur('usernameFocus')}/>
-                            <u></u>
-                            </div>
-                            <div className={`${passwordClassName} last`}>
-                            <label>Password</label>
-                            <input type="password" className="input-field" required onFocus={this.onFocus('passwordFocus')} onBlur={this.onBlur('passwordFocus')}/>
-                            <u></u>
-                            </div>
-                            <button className="btn"><span>GO</span><u></u>
-                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 415.582 415.582" xmlSpace="preserve">
-                                                    <path d="M411.47,96.426l-46.319-46.32c-5.482-5.482-14.371-5.482-19.853,0L152.348,243.058l-82.066-82.064
-                                                        c-5.48-5.482-14.37-5.482-19.851,0l-46.319,46.32c-5.482,5.481-5.482,14.37,0,19.852l138.311,138.31
-                                                        c2.741,2.742,6.334,4.112,9.926,4.112c3.593,0,7.186-1.37,9.926-4.112L411.47,116.277c2.633-2.632,4.111-6.203,4.111-9.925
-                                                        C415.582,102.628,414.103,99.059,411.47,96.426z"/>
-                                                    </svg>
+                            <InputField label={'Email'} className={`f_row ${this.shakeIfEmpty('email')}`} type='text' onChange={this.onChange('email')} errorMsg={this.state.emailErrorMsg} />
+                            <InputField label={'Password'} className={`f_row last ${this.shakeIfEmpty('password')}`} type='password' onChange={this.onChange('password')} errorMsg={this.state.passwordErrorMsg}/>
+                            <button className="btn" onClick={this.onGo}>
+                                <span>GO</span>
                             </button>
                             <div className="f_link">
-                            <a href="" className="resetTag">Forgot your password?</a>
+                                <a href="#" className="resetTag" onClick={this.onClickForgot} >Forgot your password?</a>
                             </div>
                         </form>
                         </div>
                         <div className="box forgetbox">
-                        <a href="#" className="back icon-back">
-                        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 199.404 199.404" style={{"enable-background":"new 0 0 199.404 199.404"}}
-                        xmlSpace="preserve">
-                        <polygon points="199.404,81.529 74.742,81.529 127.987,28.285 99.701,0 0,99.702 99.701,199.404 127.987,171.119 74.742,117.876 
-                            199.404,117.876 "/>
-                    </svg>
+                        <a href="#" className="back icon-back" onClick={this.onClickBack} >
+                            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 199.404 199.404" style={{"enable-background":"new 0 0 199.404 199.404"}} xmlSpace="preserve">
+                                <polygon points="199.404,81.529 74.742,81.529 127.987,28.285 99.701,0 0,99.702 99.701,199.404 127.987,171.119 74.742,117.876 199.404,117.876 "/>
+                            </svg>
                         </a>
                         <h2>Reset Password</h2>
                         <form className="form">
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
-                            <div className="f_row last">
-                            <label>Email Id</label>
-                            <input type="text" className="input-field" required/>
-                            <u></u>
-                            </div>
-                            <button className="btn"><span>Reset</span><u></u>
-                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 415.582 415.582" xmlSpace="preserve">
-                                                    <path d="M411.47,96.426l-46.319-46.32c-5.482-5.482-14.371-5.482-19.853,0L152.348,243.058l-82.066-82.064
-                                                        c-5.48-5.482-14.37-5.482-19.851,0l-46.319,46.32c-5.482,5.481-5.482,14.37,0,19.852l138.311,138.31
-                                                        c2.741,2.742,6.334,4.112,9.926,4.112c3.593,0,7.186-1.37,9.926-4.112L411.47,116.277c2.633-2.632,4.111-6.203,4.111-9.925
-                                                        C415.582,102.628,414.103,99.059,411.47,96.426z"/>
-                                                    </svg>
+                            <InputField label={'Email'} className={`f_row last ${this.shakeIfEmpty('email')}`} type='text' onChange={this.onChange('email')} errorMsg={this.state.emailErrorMsg}/>
+                            <button className="btn">
+                                <span>Reset</span>
                             </button>
                         </form>
                         </div>
@@ -127,29 +166,16 @@ export default class Login extends React.Component {
                         <span className="reg_bg"></span>
                         <h2>Register</h2>
                         <form className="form">
-                            <div className={userNameClassName}>
-                                <label>Username</label>
-                                <input type="text" className="input-field" required onFocus={this.onFocus('usernameFocus')} onBlur={this.onBlur('usernameFocus')}/>
-                                <u></u>
-                            </div>
-                            <div className={`${passwordClassName}`}>
-                            <label>Password</label>
-                            <input type="password" className="input-field" required onFocus={this.onFocus('passwordFocus')} onBlur={this.onBlur('passwordFocus')}/>
-                            <u></u>
-                            </div>
-                            <div className={`${repeatPasswordClassName} last`}>
-                            <label>Repeat Password</label>
-                            <input type="password" className="input-field" required onFocus={this.onFocus('repeatPasswordFocus')} onBlur={this.onBlur('repeatPasswordFocus')}/>
-                            <u></u>
-                            </div>
-                            <button className="btn-large">NEXT</button>
+                            <InputField label={'Email'} className={`f_row ${this.shakeIfEmpty('email')}`} type='text' onChange={this.onChange('email')} errorMsg={this.state.emailErrorMsg}/>
+                            <InputField label={'Password'} className={`f_row ${this.shakeIfEmpty('password')}`} type='password' onChange={this.onChange('password')} errorMsg={this.state.passwordErrorMsg}/>
+                            <InputField label={'Repeat Password'} className={`f_row last ${this.shakeIfEmpty('repeatPassword')}`} type='password' onChange={this.onChange('repeatPassword')} errorMsg={this.state.repeatPasswordErrorMsg}/>
+                            <button className="btn-large" onClick={this.onGo}>Go</button>
                         </form>
                         </div>
                         <a href="#" className="regTag icon-add" onClick={this.onClickRegister}>
                             <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 357 357" style={{"enable-background":"new 0 0 357 357"}} xmlSpace="preserve">
                                 <path d="M357,204H204v153h-51V204H0v-51h153V0h51v153h153V204z"/>
                             </svg>
-
                         </a>
                     </div>
                 </div>
