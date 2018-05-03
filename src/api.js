e41a9cf01752067d14d00dff6685dd073bd1218a
piscium2010 <32027562+piscium2010@users.noreply.github.com>
2018-05-03 16:21:24 +0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,3 +1,5 @@
+export { CODE } from './server/constants'
+
 const data = [
     {
         name: 'abolition',
@@ -25,6 +27,8 @@ const data = [
     }
     
 ]
+
+const host = 'http://localhost:3000/llk'
 
 export function getCourses() {
     return new Promise((res,rej)=>{
@@ -55,5 +59,41 @@ export function saveCourse() {
         setTimeout(()=>{
             res()
         },1000)
+    })
+}
+
+export function register(name, password) {
+    return new Promise(( resolve, reject ) => {
+        let xhttp = new XMLHttpRequest()
+       
+
+        let formData = new FormData()
+        formData.append('name', name)
+        formData.append('password', password)
+
+        let data = JSON.stringify({
+            name,
+            password
+        })
+
+        console.log(`data`,data)
+
+        xhttp.open('post', `${host}/register`)
+        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
+        
+
+        xhttp.onreadystatechange = function () {
+            // check if the response data send back to us 
+            if (xhttp.readyState === 4) {
+                // check if the xhttp is successful
+                if (xhttp.status === 200) {
+                    resolve(xhttp.response)
+                } else {
+                    reject(xhttp.response)
+                }
+            }
+        }
+        // specify the type of xhttp
+        xhttp.send(data)
     })
 }
