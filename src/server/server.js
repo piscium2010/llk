9bcae63f84439e62984670b240c22df45f728de3
piscium2010 <32027562+piscium2010@users.noreply.github.com>
2018-05-03 16:23:49 +0800

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,17 @@
+import { CODE } from './constants'
+
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 const session = require('express-session')
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 app.use(session({
     secret: 'keyboard cat',
@@ -10,7 +20,8 @@ app.use(session({
   }))
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 const site = 'llk'
 
 app.get(`/${site}/`,(req, res)=>{
@@ -21,9 +32,9 @@ app.get(`/${site}/`,(req, res)=>{
 
 app.put(`/${site}/addcourse`,(req, res)=>{
     let courseName = req.body.courseName
-    console.log(`addcourse session`,req.session)
-    console.log(`session.uid`,req.session.uid)
-    console.log(`session.uid`,req.session['uid'])
+    // console.log(`addcourse session`,req.session)
+    // console.log(`session.uid`,req.session.uid)
+    // console.log(`session.uid`,req.session['uid'])
 
     res.send('add cdss')
 })
@@ -31,6 +42,14 @@ app.put(`/${site}/addcourse`,(req, res)=>{
 app.post(`/${site}/addcourse`,(req, res)=>{
     console.log(`param`,req.body)
     res.send('add')
+})
+
+app.post(`/${site}/register`,(req, res)=>{
+    console.log(`registe.body`,req.body)
+    res.setHeader('Content-Type', 'text/plain')
+    setTimeout(()=>{
+        res.send(CODE.DONE)
+    },3000)
 })
 
 export default app
